8ca59e4c99ede1016add4cb68419d3f690042311
Daniel Landsman <daniellandsmanprofessional@gmail.com>
2024-09-30 08:15:33 -0400

    Update machine.h
    Added comments and updated invariant check function to remove unnecessary parameter.

--- machine.h
+++ machine.h
@@ -5,14 +5,26 @@
 
 #define MEMORY_SIZE_IN_WORDS 32768
 
+// Pre-Condition: bof represents a valid binary object file.
+// Post-Condition: Loads the file's instructions and global data
+// into memory and initializes registers.
 extern void load_bof(BOFFILE bof);
 
+// Pre-Condition: header represents a valid BOF header.
+// Post-Condition: Initializes memory to 0 and sets registers
+// to their proper starting values according to the header.
 extern void init(BOFHeader header);
 
-extern void invariant_check(BOFHeader header);
+// Pre-Condition: Registers are properly initialized and updated.
+// Post-Condition: Checks the registers to make sure all invariants hold.
+extern void invariant_check();
 
+// Pre-Condition: bof and header are a valid binary object file and header, respectively
+// Post-Condition: Loads instructions from the BOF into program memory
 void load_instrs(BOFFILE bof, BOFHeader header);
 
+// Pre-Condition: bof and header are a valid binary object file and header, respectively
+// Post-Condition: Loads global data from the BOF into program memory
 void load_globals(BOFFILE bof, BOFHeader header);
 
 #endif
